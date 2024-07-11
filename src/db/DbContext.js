d9dbb0c9292dea0c5bc7852204b7a310b68ee361
Jeremy Fowler <jembyfowler@gmail.com>
2024-07-11 10:57:50 -0600

    registered in dbcontext

--- src/db/DbContext.js
+++ src/db/DbContext.js
@@ -3,12 +3,14 @@ import { AccountSchema } from '../models/Account'
 import { ValueSchema } from '../models/Value'
 import { QuoteSchema } from '../models/Quote.js';
 import { ImageSchema } from '../models/Image.js';
+import { TodoSchema } from '../models/Todo.js';
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
   Quotes = mongoose.model('Quote', QuoteSchema);
-  Images = mongoose.model('Image', ImageSchema)
+  Images = mongoose.model('Image', ImageSchema);
+  Todos = mongoose.model('Todo', TodoSchema);
 }
 
 export const dbContext = new DbContext()
